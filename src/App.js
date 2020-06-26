da02e4d22bf410587015ca284da1f4fea619afc5
Allison Fister <allisonfister59@gmail.com>
2020-06-26 17:43:22 -0400

    resolved merge conflict

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Dashboard from './Dashboard/Dashboard';
 import logo from './logo.svg';
 import './App.css';
 import Inventory from './Inventory/Inventory';
+import Levels from "./Levels/Levels";
 
 function App() {
   return (

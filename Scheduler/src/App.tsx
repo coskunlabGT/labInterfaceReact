feaa7a1a459442b6a3f2e372556744d729b25e60
JoshPatel13 <joshptl313@gmail.com>
2020-06-29 01:40:34 -0400

    changes

--- Scheduler/src/App.tsx
+++ Scheduler/src/App.tsx
@@ -1,4 +1,4 @@
-import React,{Component} from 'react';
+/* import React,{Component} from 'react';
 import './App.css';
 import {Inject, ScheduleComponent, Day,Week,Month,Agenda} from '@syncfusion/ej2-react-schedule';
 import {DataManager,WebApiAdaptor} from '@syncfusion/ej2-data'
@@ -7,8 +7,10 @@ import * as ReactDOM from "react-dom";
 import { start } from 'repl';
 
 export default class App extends Component{
-  constructor(props:any) {
+  //window.onload = () => {
+   constructor(props:any) {
     super(props);
+    //this.action()
     this.state = {
       Title:"",
       Location:"",
@@ -16,11 +18,30 @@ export default class App extends Component{
       Start:"",
       End:"",
     }
+    
   }
+//};
 
-  componentDidMount() {
-    this.postEvent();
-  }
+action() {
+  let btn:HTMLElement=(document.getElementsByClassName("Edie-schedule-table e-content-tabletForm")[0] as HTMLElement)
+  btn.onclick=()=>(console.log("hello"));
+
+// let btn: HTMLElement = document.getElementsByClassName("e-control e-btn e-lib e-primary e-event-save e-flat")[0] as HTMLElement;
+// console.log("hello");
+//  if(btn){
+//    btn.onclick=() =>{return (this.postEvent)};
+   
+// }
+
+};
+secondaction(){
+
+};
+
+
+    componentDidMount() {
+
+    }
 
   postEvent() {
     console.log(this.state)
@@ -30,17 +51,19 @@ export default class App extends Component{
         headers: {
           'Content-Type': 'application/json',
         },
-body:JSON.stringify({
-         "summary": document.getElementById("Subject"),
-          "location": "test",
-           "description": "test",
-          "start": ,
-          "end": 
-        })
+        
+  body:JSON.stringify({
+        "summary": (document.getElementById("Subject") as HTMLInputElement).value,
+          "location": (document.getElementById("Location")as HTMLInputElement).value,
+           "description": (document.getElementById("Description") as HTMLInputElement).value,
+          "start":(document.getElementById("StartTime")as HTMLInputElement).value,
+          "end":(document.getElementById("EndTime")as HTMLInputElement).value
+        })  
+      
 
         
           
-      
+
     
 
     }
@@ -52,14 +75,10 @@ body:JSON.stringify({
         console.log(response)
 
     })
+
+    
+    
   }
-
-
- 
-
-
-
-
 
 
 
@@ -68,20 +87,125 @@ body:JSON.stringify({
     adaptor: new WebApiAdaptor,
     crossDomain: true
 
-
-
-
-
   });
   
-  public render(){
-   
 
-
+  public render() {
     return <ScheduleComponent currentView='Month'>
-      <Inject services={[Day, Week,Month,Agenda]}/>
+      <Inject services={[Day, Week,Month,Agenda]}/> 
     </ScheduleComponent>
-  }
-}
+    
+    
+    
+  } 
 
+} */
+  
+
+
+import React,{Component} from 'react';
+import './App.css';
+import {Inject, ScheduleComponent, Day,Week,Month,Agenda} from '@syncfusion/ej2-react-schedule';
+import {DataManager,WebApiAdaptor} from '@syncfusion/ej2-data'
+import { Ajax } from '@syncfusion/ej2-base';
+import * as ReactDOM from "react-dom";
+import { start } from 'repl';
+
+export default class App extends Component{
+   constructor(props:any) {
+    super(props);
+    //this.action()
+    this.state = {
+      Title:"",
+      Location:"",
+      Description:"",
+      Start:"",
+      End:"",
+    }
+    
+  }
+
+action() {
+  const cells : HTMLCollectionOf<Element> = document.getElementsByClassName("e-work-cells e-work-days");
+  for (let cell of cells as any) {
+    cell.addEventListener('click', () => {
+      //console.log("cell click");
+      //console.log(document.getElementsByClassName("e-event-create e-text-ellipsis e-control"));
+      const boxes: HTMLCollectionOf<Element> = document.getElementsByClassName("e-event-create e-text-ellipsis e-control");
+      setTimeout(() => {
+        const submitButton = boxes[0] as HTMLElement;
+        submitButton.addEventListener('click', () => {
+          const subject: HTMLInputElement = document.getElementById("Subject") as HTMLInputElement
+          const inputValue: string = subject.value
+          
+
+          console.log('submitted');
+          console.log(inputValue);
+        });
+      }, 1000);
+    });
+  }
+  //let btn:HTMLElement=(document.getElementsByClassName("Edie-schedule-table e-content-tabletForm")[0] as HTMLElement)
+  //btn.onclick=()=>(console.log("hello"));
+
+// let btn: HTMLElement = document.getElementsByClassName("e-control e-btn e-lib e-primary e-event-save e-flat")[0] as HTMLElement;
+// console.log("hello");
+//  if(btn){
+//    btn.onclick=() =>{return (this.postEvent)};
+   
+// }
+
+};
+
+secondaction(){
+
+};
+
+    componentDidMount() {
+      window.addEventListener('load', this.action);
+    }
+
+  postEvent() {
+    console.log(this.state)
+    const link = 'http://127.0.0.1:8000/calendar/api/event/';
+    let data = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        
+  body:JSON.stringify({
+        "summary": (document.getElementById("Subject") as HTMLInputElement).value,
+          "location": (document.getElementById("Location")as HTMLInputElement).value,
+           "description": (document.getElementById("Description") as HTMLInputElement).value,
+          "start":(document.getElementById("StartTime")as HTMLInputElement).value,
+          "end":(document.getElementById("EndTime")as HTMLInputElement).value
+        })  
+    }
+    
+    fetch(link, data)
+    .then(response => response.json())  // promise
+    .then(response => {
+        this.setState({event: response});
+        console.log(response)
+
+    })
+ 
+  }
+
+
+  private remoteData = new DataManager({
+    url:'https://js.syncfusion.com/demos/ejservices/api/Schedule/LoadData',
+    adaptor: new WebApiAdaptor,
+    crossDomain: true
+  });
+  
+  public render() {
+    return <ScheduleComponent currentView='Month'>
+      <Inject services={[Day, Week,Month,Agenda]}/> 
+    </ScheduleComponent>
+  
+  } 
+
+}
 
---
+++ Scheduler/src/index.jsx
@@ -0,0 +1,48 @@
+import * as React from 'react';
+import * as ReactDOM from "react-dom";
+import { ScheduleComponent, Day, Week, WorkWeek, Month, Agenda, Inject } from '@syncfusion/ej2-react-schedule';
+import { DataManager, WebApiAdaptor } from '@syncfusion/ej2-data';
+class App extends React.Component {
+    constructor() {
+        super(...arguments);
+        this.calendarId = 'joshptl313@group.calendar.google.com';
+        this.publicKey = 'AIzaSyA-Neq1tNL1WUSFRPGzMJTmxMKlYjBFG0E';
+        this.dataManger = new DataManager({
+            url: 'https://www.googleapis.com/calendar/v3/calendars/' + this.calendarId + '/events?key=' + this.publicKey,
+            adaptor: new WebApiAdaptor,
+            crossDomain: true
+        });
+    }
+    onDataBinding(e) {
+        let items = e.result.items;
+        let scheduleData = [];
+        if (items.length > 0) {
+            for (let i = 0; i < items.length; i++) {
+                let event = items[i];
+                let when = event.start.dateTime;
+                let start = event.start.dateTime;
+                let end = event.end.dateTime;
+                if (!when) {
+                    when = event.start.date;
+                    start = event.start.date;
+                    end = event.end.date;
+                }
+                scheduleData.push({
+                    Id: event.id,
+                    Subject: event.summary,
+                    StartTime: new Date(start),
+                    EndTime: new Date(end),
+                    IsAllDay: !event.start.dateTime
+                });
+            }
+        }
+        e.result = scheduleData;
+    }
+    render() {
+        return <ScheduleComponent ref={schedule => this.scheduleObj = schedule} width='100%' height='550px' selectedDate={new Date(2018, 10, 14)} readonly={true} eventSettings={{ dataSource: this.dataManger }} dataBinding={this.onDataBinding.bind(this)}>
+      <Inject services={[Day, Week, WorkWeek, Month, Agenda]}/>
+    </ScheduleComponent>;
+    }
+}
+;
+ReactDOM.render(<App />, document.getElementById('schedule'));

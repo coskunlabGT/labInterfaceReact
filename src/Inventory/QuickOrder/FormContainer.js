c2cae9cde2767078111310774392ee4336d3ed60
khusbupatel <54725189+khusbupatel@users.noreply.github.com>
2020-06-30 11:17:40 -0400

    added quickorder, inventory/order tables, & navbar (#3)
    * added quickorder, inventory/order tables, & navbar
    
    * reconfigured files and fixed nav bar
    
    * home page layout and css updates
    
    * update redirects and css

---
+++ src/Inventory/QuickOrder/FormContainer.js
@@ -0,0 +1,84 @@
+import React from 'react'
+import {API, LOCAL_HOST} from '../../Main/constants'
+import FormComponent from './FormComponent'
+import { Link } from 'react-router-dom'
+
+class Form extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            item_id: "",
+            requested_quantity: "",
+            user: "",
+            students: [],
+            items: [],
+        }
+        this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+
+    componentDidMount() {
+        fetch({API}.API + '/QuickOrder/inventory/')
+            .then((response) => response.json())
+            .then(data => {
+                let inventory = data.map(item => {
+                    return {value: item.id, display: item.item_name}
+                })
+                this.setState({
+                    items: [{value: '', display: ''}].concat(inventory)
+                })
+            }).catch(error => {
+                console.log(error);
+            })
+
+            fetch({API}.API + '/UserManagement/get-Users/')
+            .then((response) => response.json())
+            .then(data => {
+                let users = data.map(user => {
+                    return {value: user.id, display: user.name}
+                })
+                this.setState({
+                    students: [{value: '', display: ''}].concat(users)
+                })
+            }).catch(error => {
+                console.log(error);
+            })
+    }
+
+    handleChange(event) {
+        const {name, value} = event.target
+        this.setState({
+            [name] : value
+        })
+    }
+
+    handleSubmit(event) {
+        event.preventDefault()
+        const link = {API}.API+ '/QuickOrder/add-order/'
+        const data = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(this.state)
+        }
+
+        fetch(link,data)
+        .then(response => {console.log(response)})
+        .catch(error => {console.log(error)})
+        window.location = {LOCAL_HOST}.LOCAL_HOST + "/inventory/orders"
+    }
+
+    render() {
+        return(
+            <FormComponent 
+            handleChange = {this.handleChange}
+            handleSubmit = {this.handleSubmit}
+            data = {this.state}
+        />
+        )
+    }
+
+}
+
+export default Form

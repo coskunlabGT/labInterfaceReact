db89456898d0528868c08ab3e15a419ec6edb14c
Khusbu Patel <khusbu0514@gmail.com>
2020-07-09 23:58:57 -0400

    fix urls

--- src/Main/constants.js
+++ src/Main/constants.js
@@ -1,2 +1,2 @@
 export const API = 'http://lab-env.eba-4ca78nem.us-west-2.elasticbeanstalk.com'
-export const LOCAL_HOST = 'https://master.d2x2ldpv5rdax5.amplifyapp.com/'
+export const LOCAL_HOST = 'http://master.d2x2ldpv5rdax5.amplifyapp.com/'

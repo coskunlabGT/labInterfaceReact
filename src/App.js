febbb4f93b16f3cd1289bb037bc462233927ce8c
Allison Fister <allisonfister59@gmail.com>
2020-07-26 21:42:36 -0400

    adjusted css for nav bar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
 import React from 'react';
-import Dashboard from './Dashboard/Dashboard';
-import logo from './logo.svg';
-import './App.css';
 import Inventory from './Inventory/Inventory';
 import Levels from "./Levels/Levels";
 

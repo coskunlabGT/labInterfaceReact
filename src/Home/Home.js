14c57bce60ad59d20474f10b3e2a23cf7b15e0c2
Allison Fister <allisonfister59@gmail.com>
2020-08-12 13:45:26 -0400

    added Nest cameras, updated home page

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -4,8 +4,9 @@ function Home() {
     return (
         <div className = "home">
             <div className = "left">
-                <h1 id = "lab">Coskun Lab</h1>
-                <h3 id = "description"> Single Cell Biotechnology Laboratory</h3>
+                <h1 id = "lab">EZLabX</h1>
+                <h3 id = "description"> The platform designed to make lab management both easy and effective. </h3>
+                <h4 id = "credit"> Created by the Coskun Lab at Georgia Tech <span role="img">&#x1f41d;</span></h4>
             </div>
         </div>
     )

80b996bf8729987cfd723ff57276180987af1d59
Khusbu <khusbu0514@gmail.com>
2021-01-16 12:12:35 -0500

    makes all admin buttons the same

--- src/Admin/LevelManager/LevelManager.js
+++ src/Admin/LevelManager/LevelManager.js
@@ -41,7 +41,7 @@ class LevelManager extends React.Component {
     render() {
         return (
             <div>
-                <div className = "adminButtons">
+                <div className = "admin-buttons">
                     <Link to = "/admin/inventory">Inventory</Link>
                     <Link to = "/admin/users">Users</Link>
                     <Link to = "/admin/levels">Levels</Link>

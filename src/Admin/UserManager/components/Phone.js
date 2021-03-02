2ce9ed1b64c64579d88435b14d6ba20ef441977b
Khusbu <khusbu0514@gmail.com>
2021-03-02 12:19:09 -0500

    update phone field in user

--- src/Admin/UserManager/components/Phone.js
+++ src/Admin/UserManager/components/Phone.js
@@ -11,6 +11,7 @@ function Phone(props) {
                 value = {props.phone_number}
                 onChange = {props.handleChange}
                 placeholder="123-456-7890"
+                pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
             />
         </div>
     )
